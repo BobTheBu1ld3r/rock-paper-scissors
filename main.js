5e0085fe7fc70d1f143426ced4ca78e9d03aa27d
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-02-20 16:11:37 +0000

    Add core functionality
    Basic game functions added:
    - buttons to choose rock, paper or scissors
    - reset button
    - round result message
    - game result message

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
+const buttons = document.querySelectorAll("button");
+const roundResult = document.querySelector(".roundResult");
+const compScore = document.querySelector(".computerScore");
+const playScore = document.querySelector(".playerScore");
+const gameResult = document.querySelector(".gameResult");
+
+buttons.forEach(button =>{
+    button.addEventListener("click", playGame)
+})
+
+
+
 rules = [["rock", "paper"], ["paper", "scissors"], ["scissors", "rock"]];
 let playerScore = 0,
     computerScore =0;
 
-for (let i = 0; i<5; i++){
-    playGame();
-    console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
-}
 
-console.log(findResult());
+
+// for (let i = 0; i<5; i++){
+//     playGame();
+//     console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
+// }
+
+// console.log(findResult());
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+}
 
 
 function getComputerSelection() {
     return Math.floor(Math.random()*3);
-}
-
-function getPlayerSelection() {
-    let input;
-    while(!(input==="rock"||input==="scissors"||input==="paper")){
-        input = prompt("Rock, paper or scissors").toLowerCase();
-    }
-    return input;
 }
 
 
@@ -36,12 +47,26 @@ function findOutcome (playerSelection, computerSelection) {
 
 }
 
-function playGame() {
-    const playerSelection = getPlayerSelection();
-    const computerSelection = getComputerSelection();
-    console.log(`You chose ${playerSelection} and the computer chose ${rules[computerSelection][0]}`);
-    const outcome = findOutcome(playerSelection, computerSelection);
-    console.log(`You ${outcome}!`);
+function playGame(clickEvent) {
+    gameResult.textContent = "";
+    const playerSelection = clickEvent.target.getAttribute("class");
+    if(playerSelection === "reset") {
+        resetGame();
+        roundResult.textContent = "Game reset"
+    }
+    else { 
+        const computerSelection = getComputerSelection();
+        const outcome = findOutcome(playerSelection, computerSelection);
+        roundResult.textContent = `You chose ${playerSelection} and the computer chose ${rules[computerSelection][0]}. You ${outcome} the round`;
+    }
+
+    playScore.textContent = `Player score: ${playerScore}`;
+    compScore.textContent = `Computer score: ${computerScore}`;
+    
+    if(playerScore + computerScore >= 5){
+        gameResult.textContent = findResult();
+        resetGame();
+    }
 }
 
 function findResult() {
