d3c58a018035510483af33c11a860210b6cfa530
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-02-22 00:32:12 +0000

    Change query selectors to match new html

--- main.js
+++ main.js
@@ -1,72 +1,54 @@
 const buttons = document.querySelectorAll("button");
-const roundResult = document.querySelector(".roundResult");
-const compScore = document.querySelector(".computerScore");
-const playScore = document.querySelector(".playerScore");
-const gameResult = document.querySelector(".gameResult");
+const selectionMessage = document.querySelector(".selectionMessage")
+const roundResultMessage = document.querySelector(".roundResultMessage");
+const scoreMessage = document.querySelector(".scoreMessage");
 
 buttons.forEach(button =>{
     button.addEventListener("click", playGame)
 })
 
 
-
 rules = [["rock", "paper"], ["paper", "scissors"], ["scissors", "rock"]];
 let playerScore = 0,
     computerScore =0;
-
-
-
-// for (let i = 0; i<5; i++){
-//     playGame();
-//     console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
-// }
-
-// console.log(findResult());
 
 function resetGame() {
     playerScore = 0;
     computerScore = 0;
 }
 
-
 function getComputerSelection() {
     return Math.floor(Math.random()*3);
 }
 
-
 function findOutcome (playerSelection, computerSelection) {
     if(playerSelection ===  rules[computerSelection][0]) {
-        return "Draw";
+        return "We DRAW.";
     };
     if(playerSelection === rules[computerSelection][1]) {
         playerScore++;
-        return "Win";
+        return "You WIN";
     };
     computerScore++;
-    return "Loose";
+    return "You LOOSE";
 
 }
 
 function playGame(clickEvent) {
-    gameResult.textContent = "";
-    const playerSelection = clickEvent.target.getAttribute("class");
-    if(playerSelection === "reset") {
-        resetGame();
-        roundResult.textContent = "Game reset"
-    }
-    else { 
-        const computerSelection = getComputerSelection();
-        const outcome = findOutcome(playerSelection, computerSelection);
-        roundResult.textContent = `You chose ${playerSelection} and the computer chose ${rules[computerSelection][0]}. You ${outcome} the round`;
-    }
-
-    playScore.textContent = `Player score: ${playerScore}`;
-    compScore.textContent = `Computer score: ${computerScore}`;
+    roundResultMessage.textContent = "";
     
-    if(playerScore + computerScore >= 5){
-        gameResult.textContent = findResult();
-        resetGame();
-    }
+    const playerSelection = clickEvent.target.getAttribute("class");
+    const computerSelection = getComputerSelection();
+    const outcome = findOutcome(playerSelection, computerSelection);
+
+    selectionMessage.textContent = `You chose ${playerSelection}. I chose ${rules[computerSelection][0]}.`;
+    roundResultMessage.textContent = outcome;
+    scoreMessage.textContent = `hacker: ${playerScore} | mainframe: ${computerScore}`;
+    
+    // if(playerScore + computerScore >= 5){
+    //     gameResultMessage.textContent = findResult();
+    //     resetGame();
+    // }
 }
 
 function findResult() {
