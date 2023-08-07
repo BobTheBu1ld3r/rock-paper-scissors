08f3040c3f494be102884c257aa4d9b5fe032486
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-08-07 18:42:17 +0100

    Create promise-based delay function

--- main.js
+++ main.js
@@ -1,156 +1,166 @@
 const BUTTON_DELAY = 550;
 
-
 const computerText = document.querySelector(".computer-text");
 
 const buttons = document.querySelectorAll("button");
-const selectionMessage = document.querySelector(".selectionMessage")
+const selectionMessage = document.querySelector(".selectionMessage");
 const roundResultMessage = document.querySelector(".roundResultMessage");
 const scoreMessage = document.querySelector(".scoreMessage");
 
-const tryAgainScreen = document.querySelector(".try-again-screen")
-const gameResultMessage =  document.querySelector(".game-result-message");
-const tryAgainButton =  document.querySelector(".try-again-button");
-
+const tryAgainScreen = document.querySelector(".try-again-screen");
+const gameResultMessage = document.querySelector(".game-result-message");
+const tryAgainButton = document.querySelector(".try-again-button");
 
 const computerMessages = document.querySelectorAll(".computer-declaration");
 console.log(computerMessages);
 
+function type(element) {
+  element.classList.toggle("typing");
+  const message = element.dataset.message.split("");
 
-function type (element) {
-    const message = element.dataset.message.split("");
-
-    const newInterval = setInterval(function(){
-        if(message.length === 0) return clearInterval(newInterval);
-        element.textContent += message.shift();
+  return new Promise((resolve, reject) => {
+    const newInterval = setInterval(function () {
+      if (message.length === 0) {
+        clearInterval(newInterval);
+        resolve();
+      }
+      element.textContent += message.shift();
     }, 100);
+  });
 }
 
 function erase(element) {
-    const newInterval = setInterval(()=>{
-        element.textContent = element.textContent.slice(0,-1);
-    },50)
+  const newInterval = setInterval(() => {
+    element.textContent = element.textContent.slice(0, -1);
+  }, 50);
 }
 
-
-function computeTimeout (element) {
-    let LETTER_DELAY = 100;
-    let NEWLINE_DELAY = 400;
-    return element.dataset.message.length*LETTER_DELAY*1.1 + NEWLINE_DELAY;
+function computeTimeout(element) {
+  let LETTER_DELAY = 100;
+  let NEWLINE_DELAY = 400;
+  return element.dataset.message.length * LETTER_DELAY * 1.1 + NEWLINE_DELAY;
 }
 
+function delay(timeDelay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, timeDelay);
+  });
+}
 
-setTimeout(()=>{
-    computerMessages[0].classList.toggle("typing");
+delay(1000)
+  .then(() => {
     type(computerMessages[0]);
-    setTimeout(()=>{
-        computerMessages[0].classList.toggle("typing");
-        computerMessages[1].classList.toggle("typing");
-        type(computerMessages[1]);
-        setTimeout(()=>{
-            computerMessages[1].classList.toggle("typing");
-            computerMessages[2].classList.toggle("typing");
-            type(computerMessages[2]);
-            setTimeout(()=>{
-                computerMessages[2].classList.toggle("typing");
-                computerMessages[3].classList.toggle("typing");
-                type(computerMessages[3]);
-                setTimeout(()=>{
-                    computerMessages[3].classList.toggle("typing");
-                    computerMessages[4].classList.toggle("typing");
-                    type(computerMessages[4]);
-                    setTimeout(()=>{
-                        computerMessages[4].classList.toggle("typing");
-                        buttons[0].classList.toggle("hidden");
-                        setTimeout(()=>{
-                            buttons[1].classList.toggle("hidden");
-                            setTimeout(()=>{
-                                buttons[2].classList.toggle("hidden");
-                            }, BUTTON_DELAY)
-                        }, BUTTON_DELAY);
-                    }, computeTimeout(computerMessages[4])+BUTTON_DELAY);
-                }, computeTimeout(computerMessages[3]));
-            }, computeTimeout(computerMessages[2]));
-        },computeTimeout(computerMessages[1]));
-    }, computeTimeout(computerMessages[0]));
-},1000);
+    return delay(1000);
+  })
+  .then(() => {
+    type(computerMessages[1]);
+    return delay(1000);
+  });
 
-
-
-
-
+// setTimeout(() => {
+//   computerMessages[0].classList.toggle("typing");
+//   type(computerMessages[0]);
+//   setTimeout(() => {
+//     computerMessages[0].classList.toggle("typing");
+//     computerMessages[1].classList.toggle("typing");
+//     type(computerMessages[1]);
+//     setTimeout(() => {
+//       computerMessages[1].classList.toggle("typing");
+//       computerMessages[2].classList.toggle("typing");
+//       type(computerMessages[2]);
+//       setTimeout(() => {
+//         computerMessages[2].classList.toggle("typing");
+//         computerMessages[3].classList.toggle("typing");
+//         type(computerMessages[3]);
+//         setTimeout(() => {
+//           computerMessages[3].classList.toggle("typing");
+//           computerMessages[4].classList.toggle("typing");
+//           type(computerMessages[4]);
+//           setTimeout(() => {
+//             computerMessages[4].classList.toggle("typing");
+//             buttons[0].classList.toggle("hidden");
+//             setTimeout(() => {
+//               buttons[1].classList.toggle("hidden");
+//               setTimeout(() => {
+//                 buttons[2].classList.toggle("hidden");
+//               }, BUTTON_DELAY);
+//             }, BUTTON_DELAY);
+//           }, computeTimeout(computerMessages[4]) + BUTTON_DELAY);
+//         }, computeTimeout(computerMessages[3]));
+//       }, computeTimeout(computerMessages[2]));
+//     }, computeTimeout(computerMessages[1]));
+//   }, computeTimeout(computerMessages[0]));
+// }, 1000);
 
 console.log(tryAgainButton.getAttribute("class"));
 
+buttons.forEach((button) => {
+  // if(button.getAttribute("class").includes())
+  button.addEventListener("click", playGame);
+});
 
-buttons.forEach(button =>{
-    // if(button.getAttribute("class").includes())
-    button.addEventListener("click", playGame)
-})
-
-
-
-rules = [["rock", "paper"], ["paper", "scissors"], ["scissors", "rock"]];
+rules = [
+  ["rock", "paper"],
+  ["paper", "scissors"],
+  ["scissors", "rock"],
+];
 let playerScore = 0,
-    computerScore =0;
+  computerScore = 0;
 
 function resetGame() {
-    playerScore = 0;
-    computerScore = 0;
-    tryAgainScreen.style.display = "none";
-    selectionMessage.textContent = "";
-    roundResultMessage.textContent = "";
-    scoreMessage.textContent = "";
-
+  playerScore = 0;
+  computerScore = 0;
+  tryAgainScreen.style.display = "none";
+  selectionMessage.textContent = "";
+  roundResultMessage.textContent = "";
+  scoreMessage.textContent = "";
 }
 
 function getComputerSelection() {
-    return Math.floor(Math.random()*3);
+  return Math.floor(Math.random() * 3);
 }
 
-function findOutcome (playerSelection, computerSelection) {
-    if(playerSelection ===  rules[computerSelection][0]) {
-        return "We DRAW.";
-    };
-    if(playerSelection === rules[computerSelection][1]) {
-        playerScore++;
-        return "You WIN";
-    };
-    computerScore++;
-    return "You LOOSE";
-
+function findOutcome(playerSelection, computerSelection) {
+  if (playerSelection === rules[computerSelection][0]) {
+    return "We DRAW.";
+  }
+  if (playerSelection === rules[computerSelection][1]) {
+    playerScore++;
+    return "You WIN";
+  }
+  computerScore++;
+  return "You LOOSE";
 }
 
 function playGame(clickEvent) {
-    roundResultMessage.textContent = "";
-    
-    const playerSelection = clickEvent.target.getAttribute("class");
+  roundResultMessage.textContent = "";
 
-    if(playerSelection === "try-again-button") return resetGame();
+  const playerSelection = clickEvent.target.getAttribute("class");
 
-    const computerSelection = getComputerSelection();
-    const outcome = findOutcome(playerSelection, computerSelection);
+  if (playerSelection === "try-again-button") return resetGame();
 
-    selectionMessage.textContent = `You chose ${playerSelection}. I chose ${rules[computerSelection][0]}.`;
-    roundResultMessage.textContent = outcome;
-    scoreMessage.textContent = `hacker: ${playerScore} | mainframe: ${computerScore}`;
+  const computerSelection = getComputerSelection();
+  const outcome = findOutcome(playerSelection, computerSelection);
 
-    if(isOver(playerScore, computerScore)){
-        tryAgainScreen.style.display = "flex";
-        [gameResultMessage.textContent, tryAgainButton.textContent] = findResult();
-    }
+  selectionMessage.textContent = `You chose ${playerSelection}. I chose ${rules[computerSelection][0]}.`;
+  roundResultMessage.textContent = outcome;
+  scoreMessage.textContent = `hacker: ${playerScore} | mainframe: ${computerScore}`;
+
+  if (isOver(playerScore, computerScore)) {
+    tryAgainScreen.style.display = "flex";
+    [gameResultMessage.textContent, tryAgainButton.textContent] = findResult();
+  }
 }
 
-function isOver (playerScore, computerScore) {
-    return playerScore + computerScore >= 5;
+function isOver(playerScore, computerScore) {
+  return playerScore + computerScore >= 5;
 }
 
 function findResult() {
-    if(playerScore > computerScore){
-        return ["YOU WIN THE GAME", "Play again"];
-    }
-    else if (playerScore === computerScore){
-        return ["You Draw! Not too shabby", "Play again"];
-    }
-    return ["HA, YOU LOST THE GAME.", "Try again"];
+  if (playerScore > computerScore) {
+    return ["YOU WIN THE GAME", "Play again"];
+  } else if (playerScore === computerScore) {
+    return ["You Draw! Not too shabby", "Play again"];
+  }
+  return ["HA, YOU LOST THE GAME.", "Try again"];
 }
